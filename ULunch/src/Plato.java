24ec556f253e7bcfc8ddda32c258910d3fcce05b
ruzafa8 <ruzafa-@outlook.es>
2019-06-02 14:04:41 +0200

    Actualizada clase Plato con override equals(Object) : boolean y override hashCode() : int

--- ULunch/src/Plato.java
+++ ULunch/src/Plato.java
@@ -2,13 +2,13 @@ import java.util.LinkedList;
 import java.util.List;
 
 public class Plato {
-	
+
 	private String nombre;
 	private String imagen;
 	private Dieta dieta;
 	private List<Alergias> alergias;
 	private List<Valoracion> valoracion;
-	
+
 	public Plato(String nombre, String imagen, Dieta dieta, List<Alergias> alergias) {
 		this.nombre = nombre;
 		this.imagen = imagen;
@@ -16,12 +16,12 @@ public class Plato {
 		this.alergias = alergias;
 		this.valoracion = new LinkedList<Valoracion>();
 	}
-	
+
 	public void addValoracion(Usuario user, int puntuacion, String comentario){
 		Valoracion val = new Valoracion(user, puntuacion, comentario);
 		valoracion.add(val);
 	}
-	
+
 	public double getPuntuacionMedia() {
 		double media = 0;
 		for(Valoracion val : valoracion) {
@@ -29,24 +29,38 @@ public class Plato {
 		}
 		return media/valoracion.size();
 	}
-	
+
 	public List<Valoracion> getValoraciones() {
 		return valoracion;
 	}
-	
+
 	public Dieta getDieta() {
 		return dieta;
 	}
-	
+
 	public List<Alergias> getAlergias(){
 		return alergias;
 	}
-	
+
 	public String getNombre() {
 		return nombre;
 	}
-	
+
 	public String getImagen() {
 		return imagen;
+	}
+
+	@Override
+	public boolean equals(Object obj){
+		boolean res = obj instanceof Plato
+		Plato plato = res?(Plato)obj:null;
+		return plato != null && getNombre().equalsIgnoreCase(plato.getNombre())
+			&& getDieta() == plato.getDieta()
+			&& getAlergias().equals(plato.getAlergias());
+	}
+	@Override
+	public int hashCode(){
+		return getNombre().hashCodeIgnoreCase() +
+			getDieta() + getAlergias().hashCode();
 	}
 }
